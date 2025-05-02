4d91413471fbfdf16d971db93d6b67f21bd1f705
Captain-JKM <alfonso.vicente@jala.university>
2025-05-02 06:51:18 -0600

    feat(app): Update cors settings

--- src/app.js
+++ src/app.js
@@ -9,7 +9,7 @@ import galleryRoutes from './routes/galery.routes.js';
 const app = express();
 
 app.use(cors({
-    origin: ['https://jeim-music.vercel.app', 'https://captaingroup.online/'],
+    origin: ['https://jeim-music.vercel.app', 'https://captaingroup.online/', 'https://djboard.vercel.app/'],
     credentials: true,
     
 }));
